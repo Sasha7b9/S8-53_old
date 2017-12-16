f56e662e84ecbd7e61db4a157ed101c133fa3dcd
Sasha <Sasha7b9@tut.by>
2017-12-16 22:39:00 +0300

    Компилируются проекты для Visual Studio

--- sources/libs/common/_VS/stdlib.h
+++ sources/libs/common/_VS/stdlib.h
@@ -1,10 +1,7 @@
 #pragma once
 
-void memcpy(void *, const void *, int);
-void memset(void *, int, int);
 void *malloc(int);
 void free(void *);
-int memcmp(void *, const void *, int);
 #define RAND_MAX 0x7fffffff
 
 
