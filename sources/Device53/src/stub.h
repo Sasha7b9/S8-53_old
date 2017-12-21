672705fb51e2b3c60957f583e91283318c98b3a0
Sasha7b9 <Sasha7b9@tut.by>
2017-12-21 11:44:02 +0300

    Рефакторинг

--- sources/Device53/src/stub.h
+++ sources/Device53/src/stub.h
@@ -3,8 +3,6 @@
 #include "defines.h"
 
 
-#define PageDisplay_Init()
-#define COLOR_SCHEME_IS_WHITE_LETTERS       true
 #define SHOW_STRING_NAVI_TEMP               false
 #define Measures_ShorPressOnSmallButtonSettings()
 #define Measure_ShortPressOnSmallButonMarker()
