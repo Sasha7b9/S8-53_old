618712cad660e7574243316d06d36c0d1086b4df
Sasha7b9 <Sasha7b9@tut.by>
2018-01-12 16:08:20 +0300

    Рефакторинг

--- sources/Device53/src/Display/Display.cpp
+++ sources/Device53/src/Display/Display.cpp
@@ -1847,8 +1847,8 @@ void Display::DrawMeasures()
         int y0 = MEAS_POS_CUR_U0 + GRID_TOP;
         int x1 = MEAS_POS_CUR_T1 - SHIFT_IN_MEMORY + Grid::Left();
         int y1 = MEAS_POS_CUR_U1 + GRID_TOP;
-        SortInt(&x0, &x1);
-        SortInt(&y0, &y1);
+        Sort(&x0, &x1);
+        Sort(&y0, &y1);
         Painter::DrawRectangle(x0, y0, x1 - x0, y1 - y0, Color::FILL);
     }
 
