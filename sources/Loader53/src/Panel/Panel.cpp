829d1ff6587533e254a29b1ea6a92b464c708b14
Sasha7b9 <Sasha7b9@tut.by>
2018-02-15 10:08:19 +0300

    Loader Компилируется с отключенными предупреждениями и работает

--- sources/Loader53/src/Panel/Panel.cpp
+++ sources/Loader53/src/Panel/Panel.cpp
@@ -5,7 +5,7 @@
 #include "Settings/Settings.h"
 #include "Display/Display.h"
 #include "Hardware/Timer.h"
-#include "interrupts.h"
+#include "Hardware/it.h"
 #include <stdio.h>
 #include <string.h>
 
